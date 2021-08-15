f97f1cb75eff22849510992f8858552d7387a32c
cskpsuganya <88704038+cskpsuganya@users.noreply.github.com>
2021-08-15 11:19:15 +0530

    Create redux.jsx

---
+++ frontreact/src/app/config/redux.jsx
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+
+import { userReducer } from '../redux/user';
+import { coursesReducer } from '../redux/courses';
+import { courseReducer } from '../redux/course';
+
+export default combineReducers({
+    user: userReducer,
+    courses: coursesReducer,
+    course: courseReducer,
+});
