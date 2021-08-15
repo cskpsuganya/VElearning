abaa979fe4c38f5a9795b87ad19b293a3b19715d
cskpsuganya <88704038+cskpsuganya@users.noreply.github.com>
2021-08-15 11:21:34 +0530

    Create store.jsx

---
+++ frontreact/src/app/config/store.jsx
@@ -0,0 +1,21 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
+import { createLogger } from 'redux-logger';
+
+import reducer from './redux';
+import rootSaga from './sagas';
+
+const logger = createLogger({
+    collapsed: true,
+});
+
+const sagaMiddleware = createSagaMiddleware();
+
+const composeEnhancers = composeWithDevTools({ trace: true, traceLimit: 25 });
+
+const storeObj = createStore(reducer, composeEnhancers(applyMiddleware(sagaMiddleware, logger)));
+
+sagaMiddleware.run(rootSaga);
+
+export const store = storeObj;
